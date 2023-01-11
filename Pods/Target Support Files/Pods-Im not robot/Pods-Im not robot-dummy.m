4b812ed38799d56041843923fe105b36e892de73
mikhailovaNV <fefu.mikhailova@feip.school>
2023-01-11 13:27:23 +0700

    proj

---
+++ Pods/Target Support Files/Pods-Im not robot/Pods-Im not robot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Im_not_robot : NSObject
+@end
+@implementation PodsDummy_Pods_Im_not_robot
+@end
---
+++ UIComponents/Color.swift
@@ -0,0 +1,35 @@
+
+import Foundation
+import UIKit
+
+class Pallete {
+    static var blueColor: UIColor = UIColor.hex("#0ba1ff")
+    static var fillSecondary: UIColor = UIColor.hex("#787880")
+    static var grayLabel: UIColor = UIColor.hex("#788ead")
+    static var lightGray: UIColor = UIColor.hex("#D3D3D3")
+    static var lightRed: UIColor = UIColor.hex("#cf7986")
+}
+
+extension UIColor {
+    static func hex(_ hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
